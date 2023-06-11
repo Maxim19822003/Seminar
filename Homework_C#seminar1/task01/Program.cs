f1c5fe6617fec60ff889efcde7db10eecbfcb629
maxim.voronin <mak1982vor@yandex.ru>
2023-06-11 22:48:12 +0300

    add +  decision task 01

--- Homework_C#seminar1/task01/Program.cs
+++ Homework_C#seminar1/task01/Program.cs
@@ -7,12 +7,15 @@
 */
 
 Console.WriteLine("Введите первое число: ");
+
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
+
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите третье число: ");
+
 int thirdNumber = Convert.ToInt32(Console.ReadLine());
 
 if (firstNumber > secondNumber)
