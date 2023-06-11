0cef074bbd653c45521ee38ee1ecc1983a83e1be
maxim.voronin <mak1982vor@yandex.ru>
2023-06-11 21:04:59 +0300

    task 3

---
+++ Classmate_C#seminar1/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Введите число: 8
+-8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8
+*/
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+//int n1 = Convert.ToInt32(n * -1);
+for (int i = -n; i <= n; i++)
+{
+    Console.Write($"{i} ");
+}
+
