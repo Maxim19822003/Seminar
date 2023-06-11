d68d6fdd1a01d16934f55ccac60446877d4928e0
maxim.voronin <mak1982vor@yandex.ru>
2023-06-11 20:56:13 +0300

    task2

---
+++ Classmate_C#seminar1/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+День недели по номеру:
+3 -> среда
+*/
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Dictionary<int, string> map = new Dictionary<int, string>()
+{
+    { 1, "Понедельник"},
+    { 2, "Вторник"},
+    { 3, "Среда"},
+    { 4, "Четверг"},
+    { 5, "Пятница"},
+    { 6, "Суббота"},
+    { 7, "Воскресение!!!"}
+};
+if (n > 0 && n < 8)
+{
+    Console.WriteLine(map[n]);
+}
+else
+{
+    Console.WriteLine($"Ошибка!");
+}
+
+
