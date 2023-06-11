b80f9f74d8ec9b93b971ac98f0b8e1dafb8d0246
maxim.voronin <mak1982vor@yandex.ru>
2023-06-11 19:36:48 +0300

    task 0

--- Classmate_C#seminar1/task0/Program.cs
+++ Classmate_C#seminar1/task0/Program.cs
@@ -1,1 +1,13 @@
 ﻿/*
+Напишите программу,которая на вводе принимает
+два числа, а на выходе даёт их квадрат.
+Например:
+4 -> 16
+*/
+Console.Write("Input number => ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int squareNumber = number * number;
+
+Console.WriteLine(squareNumber);
+
