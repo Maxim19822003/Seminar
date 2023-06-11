eca7afbc68348db233394aa7325b8964727550a1
maxim.voronin <mak1982vor@yandex.ru>
2023-06-11 20:06:14 +0300

    (int)

--- Classmate_C#seminar1/task0/Program.cs
+++ Classmate_C#seminar1/task0/Program.cs
@@ -8,6 +8,6 @@ Console.Write("Input number => ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int power = 2; // power of number
-int squareNumber = Convert.ToInt32(Math.Pow(number, power));
+int squareNumber = (int)Math.Pow(number, power);
 Console.WriteLine($"Square {number} is {squareNumber}");
 
