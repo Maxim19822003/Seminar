303484692b2b60818d0b67bb4c02f64dbcaa5262
maxim.voronin <mak1982vor@yandex.ru>
2023-06-11 21:14:34 +0300

    task 4

---
+++ Classmate_C#seminar1/task4/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ВВодится трёхзначное число, выводится последнее, например:
+875 -> 5
+*/
+
+Console.Write("Введите трёхзначное число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(n % 10);
