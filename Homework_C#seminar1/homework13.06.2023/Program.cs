1b6b17cf7826e56f52cf851c6901dabf629c6e92
maxim.voronin <mak1982vor@yandex.ru>
2023-06-14 22:20:51 +0300

    task 10

---
+++ Homework_C#seminar1/homework13.06.2023/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Write("Enter a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Second number is {number/10%10}");
+/*
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+
+int firstNum = number/10;
+int secondNum = firstNum%10;
+
+Console.WriteLine($"Our random number is {number}");
+Console.WriteLine($"Second number is {secondNum}");
+*/
+
+/*
+Задача 13: Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+
+Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
