affee923b7e5c08eeb352e80393fe297095836b3
maxim.voronin <mak1982vor@yandex.ru>
2023-06-14 19:24:04 +0300

    task 6

---
+++ Classmate_C#seminar1/task6/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая выводит
+ случайное трехзначное число и 
+ удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+
+
+int firstNum = number/100;
+int secondNum = number%10;
+
+Console.WriteLine($"Our random number is {number}");
+Console.WriteLine($"First number is {firstNum}");
+Console.WriteLine($"Second number is {secondNum}");
