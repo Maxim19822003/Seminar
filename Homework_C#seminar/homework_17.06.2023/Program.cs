685b21c225bb70b9edeeb3ff087b12908f6bf31d
maxim.voronin <mak1982vor@yandex.ru>
2023-06-18 23:06:43 +0300

    task 21

--- Homework_C#seminar/homework_17.06.2023/Program.cs
+++ Homework_C#seminar/homework_17.06.2023/Program.cs
@@ -2,7 +2,8 @@
 Знакомство с языками программирования (семинары)
 Урок 3. Базовые алгоритмы. Продолжение
 Задача 19
-Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Напишите программу, которая принимает на вход пятизначное число
+и проверяет, является ли оно палиндромом.
 14212 -> нет
 12821 -> да
 23432 -> да
@@ -10,9 +11,38 @@
 
 /*
 Задача 21
-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Напишите программу, которая принимает на вход координаты
+двух точек и находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+/*
+void Print(string text)
+{
+    Console.WriteLine(text);
+}
+
+int GetIntNumber()
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+double FindDistance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double result = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+    return result;
+}
+
+Print("Input one by one x1, y1, x2, y2, z1, z2:");
+int x1 = GetIntNumber();
+int y1 = GetIntNumber();
+int z1 = GetIntNumber();
+int x2 = GetIntNumber();
+int y2 = GetIntNumber();
+int z2 = GetIntNumber();
+
+double result = FindDistance(x1, y1, z1, x2, y2, z2);
+Print($"Distance between points is: {result} ");
 */
 
 /*
@@ -22,7 +52,6 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
-
 /*
 int RandomNumber(int min, int max){
     int result = new Random().Next(min, max+1);
