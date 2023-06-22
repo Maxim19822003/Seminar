fb6975511b9f40d45ffa8b21bbaf7a33451f05d4
maxim.voronin <mak1982vor@yandex.ru>
2023-06-22 13:29:59 +0300

    22.06.2023.1

--- Classwork_C#seminar/task12/Program.cs
+++ Classwork_C#seminar/task12/Program.cs
@@ -4,7 +4,7 @@
 4 -> 10
 8 -> 36
 */
-
+/*
 int GetNumber(string description)
 {
 
@@ -17,13 +17,25 @@ int GetNumber(string description)
         {
             return number;
         }
-        Console.Write($"This number {temp} is not correct. Try again =>");
+        Console.Write($"This number \"{temp}\" is not correct. Try again =>");
     }
-    Console.WriteLine(GetNumber("Input number => "));
-
+    
 
 }
+ int GetSum(int number){
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+ }
+ int number = GetNumber("Input number ");
 
+Console.WriteLine($"Sum number from 1 to {number} is {GetSum(number)}.");
+*/
+//int [] array = {4, 5, 8, 9};
+//Console.WriteLine($"Our array => [{string.Join(", ", array)}]");
 /*
 Задача 26: Напишите программу, которая принимает
 на вход число и выдаёт количество цифр в числе.
@@ -103,9 +115,46 @@ int mult = GetMultiplication(number);
 
 Console.WriteLine($"Result is {mult}");
 */
+/*
+int GetNumber(string description){
 
+    int number;
+    Console.Write($"{description} => ");
+
+    while(true){
+        string temp = Console.ReadLine();
+        if(int.TryParse(temp, out number)){
+            return number;
+        }
+        Console.Write($"This number {temp} is not correct. Try again. => ");
+    }
+    
+}
+int number = GetNumber("Input number: ");
+int ProductOfANumber(int number){
+    int multiplication = 1;
+    for (int i = 1 ; i <= number ; i++){
+        multiplication *= i;
+    }
+    return multiplication;
+}
+
+Console.WriteLine(ProductOfANumber(number));
+*/
 /*
 Задача 30: Напишите программу, которая выводит массив из 8 элементов,
 заполненный нулями и единицами в случайном порядке.
 [1,0,1,1,0,1,0,0]
 */
+void FillArray(int[] collection){
+
+    int length = collection.Length;
+    int index = 0;
+    while(index<length){
+        collection[index] = new Random().Next(0, 2);
+        index ++;
+    }
+    Console.WriteLine($"[{string.Join(", ", collection)}]");
+}
+int[] arr = new int[8];
+FillArray(arr);
